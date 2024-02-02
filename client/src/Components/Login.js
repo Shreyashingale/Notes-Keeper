550be03af28232deca375ada4e8720ae8a457845
ShreyashIngale <shreyashingale282@gmail.com>
2024-02-02 18:22:11 +0530

    link added

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -21,7 +21,7 @@ const Register = () => {
 
     async function loginUser(event) {
         event.preventDefault()
-        const response = await fetch('http://notes-keeper-nine.vercel.app/api/login', {
+        const response = await fetch('https://notes-keeper-nine.vercel.app/api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
